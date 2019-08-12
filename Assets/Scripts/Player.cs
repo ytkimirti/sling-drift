da311e5704142b84e08e448b871ddce5d6c6c165
ytkimirti <yusuftaha9@gmail.com>
2019-08-12 18:29:10 +0300

    Simple car took off

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
+
+    public float moveSpeed;
+    public float accelerateSpeed;
+
+    [HideInInspector]
+    public Rigidbody rb;
+
+    bool isTookOff;
 
     public static Player main;
 
@@ -14,11 +24,22 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        if (!isTookOff && rb.velocity.z < moveSpeed)
+        {
+            rb.velocity = rb.velocity + (Vector3.forward * Time.deltaTime * accelerateSpeed);
+
+            if (rb.velocity.z > moveSpeed)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, 0, moveSpeed);
+                isTookOff = true;
+            }
+        }
+
 
     }
 }
---
+++ Assets/Scripts/TestCode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCode : MonoBehaviour
+{
+    public float velocity;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(Input.GetAxis("Horizontal") * velocity, 0,
+                                                         Input.GetAxis("Vertical") * velocity);
+    }
+}
